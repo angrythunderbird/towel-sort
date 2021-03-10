a699d277752e101cc2cc7ec1b9344924d0e815df
angrythunderbird <headcrab.albert@gmail.com>
2021-03-11 01:30:34 +0200

    feat: it finally work after node 12 install, hoooh

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+  
+  matrix.map(function (current, index) {
+    index % 2 != 0 ? current.reverse() : current;
+  });
+
+  return matrix.flat();
+};
